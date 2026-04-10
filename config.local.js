c2967ed1a152696abea334966486ac4d78b07a34
Florian Wachter <6311736+FloWachter@users.noreply.github.com>
2026-04-10 14:45:54 +0200

    Restrict CORS origins to specific domains

--- config.local.js
+++ config.local.js
@@ -2,6 +2,9 @@ export default {
   PORT: process.env.PORT || 8061,
   CACHE_ENGINE: 'node-cache',
   CORS: {
-    origins: ['*']
+    origins: [
+      'http://localhost:3004',
+      'https://oyfora.io',
+    ]
   }
 };
