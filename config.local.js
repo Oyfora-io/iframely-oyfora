a44fff88f3714471cc09cd1f69e3c237a77c0463
Florian Wachter <6311736+FloWachter@users.noreply.github.com>
2026-04-10 13:52:29 +0200

    Iframely repo is being run as an ES module

--- config.local.js
+++ config.local.js
@@ -1,16 +1,7 @@
-module.exports = {
+export default {
   PORT: process.env.PORT || 8061,
-
   CACHE_ENGINE: 'node-cache',
-  CACHE_TTL: 60 * 60 * 1000,
-
-  // tighten this to your app domain later
   CORS: {
     origins: ['*']
-  },
-
-  // optional: set if you use provider APIs later
-  providerOptions: {
-    locale: 'en_US'
   }
 };
