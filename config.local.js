2c2e2a8cc612a5b8781d664849a15752347e20db
Florian Wachter <6311736+FloWachter@users.noreply.github.com>
2026-04-10 13:22:36 +0200

    updated config-local file

--- config.local.js
+++ config.local.js
@@ -1,1 +1,16 @@
+module.exports = {
+  PORT: process.env.PORT || 8061,
 
+  CACHE_ENGINE: 'node-cache',
+  CACHE_TTL: 60 * 60 * 1000,
+
+  // tighten this to your app domain later
+  CORS: {
+    origins: ['*']
+  },
+
+  // optional: set if you use provider APIs later
+  providerOptions: {
+    locale: 'en_US'
+  }
+};
